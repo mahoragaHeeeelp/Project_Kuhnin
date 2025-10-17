47ce2db3133d6cf91940e25d41f43ea94a7c497b
FakeMykka <228510391+mahoragaHeeeelp@users.noreply.github.com>
2025-10-18 02:57:45 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,1 +1,8 @@
-
+try:
+    price = float(input())
+    weight = 1.2
+    while weight <= 2.0:
+        print(f"{weight} кг: {price * weight:}")
+        weight += 0.2
+except ValueError:
+    print("ошибка, жалко")
