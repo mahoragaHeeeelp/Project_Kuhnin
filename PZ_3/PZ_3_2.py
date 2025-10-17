01605534efcb29cc925cecb8badf9ee5b6dbb786
FakeMykka <228510391+mahoragaHeeeelp@users.noreply.github.com>
2025-10-18 02:53:18 +0300

    Update PZ_3_2.py

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -1,1 +1,14 @@
+try:
+    num = int(input("введите трёхзначное число: "))
+    suit = num // 100
+    rank = num % 100
 
+    suits = {1: "пик", 2: "треф", 3: "бубен", 4: "червей"}
+    ranks = {11: "валет", 12: "дама", 13: "король", 14: "туз"}
+
+    if suit in suits and rank in ranks:
+        print(f"{ranks[rank]} {suits[suit]}")
+    else:
+        print("неверный номер карты")
+except ValueError:
+    print("ошибка, как так то")
