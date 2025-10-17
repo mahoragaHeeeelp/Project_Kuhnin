6d7dfce5824bd1fa6b3abd91faca7ba4f69223c1
FakeMykka <228510391+mahoragaHeeeelp@users.noreply.github.com>
2025-10-18 02:59:06 +0300

    Update PZ_4_2.py

--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,1 +1,12 @@
-
+try:
+    n = int(input())
+    count = 0
+    total = 0
+    while n > 0:
+        digit = n % 10
+        total += digit
+        count += 1
+        n //= 10
+    print(count, total)
+except ValueError:
+    print("эхх, ошибка")
