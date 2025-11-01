b55589382a17e47de192ca2b53a8750e19f6ce1b
FakeMykka <228510391+mahoragaHeeeelp@users.noreply.github.com>
2025-11-01 14:03:09 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,1 +1,27 @@
+def cc (s):
+    c = 0
+    for i in s:
+        c+= 1
+    return c
+
+try:
+    W = str(input("слово-")
+            if w = "":
+                print("ошибка")
+            else:
+                n = cc(w)
+                top = '#' * (n+4)
+                mid = '# ' + w + ' #'
+                print(top)
+                print(mid)
+                print(top)
+
+except ValueError:
+    print("ошибка")
+                
+
+
+
+
+
 
