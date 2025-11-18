4b1f99d44ee87321409c672808e14e622905544f
FakeMykka <228510391+mahoragaHeeeelp@users.noreply.github.com>
2025-11-19 00:37:57 +0300

    Update PZ_6_1.py

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,1 +1,16 @@
+try:
+    N = int(input("Введите N (>2): "))
+    if N <= 2:
+        raise ValueError("N должно быть больше 2")
+    
+    fib = [1, 1]
+    
+    for i in range(2, N):
+        next_fib = fib[i-2] + fib[i-1]
+        fib.append(next_fib)
+    
+    print("первые", N, "чисел фибоначчи:")
+    print(fib)
 
+except Exception as e:
+    print("ошибка: {e}")
