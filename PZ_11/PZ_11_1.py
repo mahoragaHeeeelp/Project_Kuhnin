4bc684315bb0948a2d039c6c0c25bcfcdf677507
skibidi sigma i love rksi <228510391+mahoragaHeeeelp@users.noreply.github.com>
2026-03-27 12:22:01 +0300

    Update PZ_11_1.py

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -8,7 +8,7 @@ def solve_task_1():
     n_list = list(map(int, input("введите числа через пробел: ").split()))
     
     if len(n_list) < 2:
-        print("нужно хотя бы два числа!")
+        print("нужно хотя бы два числа")
         return
 
     result = [(n_list[i] + n_list[i+1])**2 for i in range(len(n_list) - 1)]
