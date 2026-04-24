1288424995673f71f0fd4818340232efc9151875
skibidi sigma i love rksi <228510391+mahoragaHeeeelp@users.noreply.github.com>
2026-04-24 17:14:10 +0300

    Update PZ_12_1.py

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -3,19 +3,19 @@
 В матрице элементы второго столбца возвести в квадрат.
 """
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+import random
 
-print("исходная матрица")
+rows = int(input("введите количество строк "))
+cols = int(input("введите количество столбцов "))
+
+matrix = list(map(lambda i: list(map(lambda j: random.randint(1, 20), range(cols))), range(rows)))
+
+print("исходная матрица ")
 for row in matrix:
     print(row)
 
-for i in range(len(matrix)):
-    matrix[i][1] = matrix[i][1] ** 2
+matrix = list(map(lambda row: row[:1] + [row[1] ** 2] + row[2:] if len(row) > 1 else row, matrix))
 
-print("новая матрица")
+print("новая матрица ")
 for row in matrix:
     print(row)
