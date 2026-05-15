30b2bce781f8055476bf89dffeaaec46141acb99
skibidi sigma i love rksi <228510391+mahoragaHeeeelp@users.noreply.github.com>
2026-05-16 02:48:50 +0300

    Update PZ_13.py

--- PZ_13/PZ_13.py
+++ PZ_13/PZ_13.py
@@ -16,7 +16,7 @@ zero_ips = list(filter(lambda ip: ip.endswith('.0'), all_ips))
 other_ips = list(filter(lambda ip: not ip.endswith('.0'), all_ips))
 
 with open('first_file.txt', 'w', encoding='utf-8') as f1:
-    f1.write('\n'.join(zero_ips) + 'количество строк: {len(zero_ips)}')
+    f1.write('\n'.join(zero_ips) + f'\nКоличество строк: {len(zero_ips)}')
 
 with open('second_file.txt', 'w', encoding='utf-8') as f2:
-    f2.write('\n'.join(other_ips) + 'количество строк: {len(other_ips)}')
+    f2.write('\n'.join(other_ips) + f'\nКоличество строк: {len(other_ips)}')
